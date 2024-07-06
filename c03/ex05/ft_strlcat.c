db0f3f8d8e63f9211c8bb60b1722c36809ed232f
Julia Russell <julrusse@c2r5s3.42lausanne.ch>
2024-07-06 17:03:21 +0200

    c03

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -1,4 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julrusse <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 10:15:45 by julrusse          #+#    #+#             */
+/*   Updated: 2024/07/04 10:34:30 by julrusse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
+
+unsigned int	ft_count(char *str)
+{
+	unsigned int	x;
+
+	x = 0;
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
@@ -6,13 +30,8 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	unsigned int	i_src;
 	unsigned int	i;
 
-	i_dest = 0;
-	i_src = 0;
-	while ((dest[i_dest] != '\0') || (src[i_src] != '\0'))
-	{
-		i_dest++;
-		i_src++;
-	}
+	i_dest = ft_count(dest);
+	i_src = ft_count(src);
 	if (size <= i_dest)
 	{
 		return (size + i_src);
